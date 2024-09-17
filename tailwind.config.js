c591960ca232917a4e433b71b26e2fe8940a9e28
danjklein <113483010+danjklein@users.noreply.github.com>
2024-09-17 14:24:59 -0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import(import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme:{
+    extend: {},
+  },
+  plugins: [],
+};
+  
